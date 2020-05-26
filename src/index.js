01b6d17ea148a6a6a2a960f687cef840cdcb7d16
lpr <791226405@qq.com>
2020-05-27 00:56:30 +0800

    css-loader style-loader scss-loader postcss-loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-// webpack 模块打包工具
+// webpack是模块打包工具
 
-// CommonJS引入方式
-let Header =require('./header.js')
+let Header = require('./header.js') // CommonJS引入方式
+import Content from './content.js' // ES Moudule 模块引入方式
 
-// ES Moudule 模块引入方式
-import Content from './content.js'
+// let avatar = require('../assets/avatar.jpg')
+import avatar from '../assets/avatar.jpg'
+import './index.scss'
+let img = new Image()
+img.src = avatar
+img.classList.add('avatar')
+let dom = document.getElementById('root')
+dom.append(img)
+
+console.log(avatar)
 
 new Header()
 new Content()
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path')
 module.exports = {
-    entry: './src/index.js',
+    mode: 'production', // 默认
+    entry: {
+        main: './src/index.js',
+    },
     output: {
         filename: 'main.js',
         // __dirname 当前webpack.config.js文件所在的目录
-        path: path.resolve(__dirname,'dist')
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.jpg$/,
+            // file-loader
+            // loader: 'file-loader',
+            // options: {
+            //     name:'[name].[ext]',
+            //     outputPath: 'images/',
+            // },
+            // url-loader 图片很小用url-loader
+            use: {
+                loader: 'url-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images/',
+                    // 单位:字节
+                    limit: 2048
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.scss$/,
+            // 执行顺序 从上到下，从右到左
+            use: ['style-loader', 'css-loader','sass-loader','postcss-loader']
+        }]
     }
 }
